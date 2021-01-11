2f05071590b4d477c34fb03b1ea2d45173115451
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2021-01-11 12:12:17 -0600

    Inventory properties via Assembly schema on bmcweb
    This commit implements Redfish Assembly schema on bmcweb.
    This schema will be used to publish inventory data for FRUs which are
    attached to a given Chassis and does not map to any specific schema
    definition.
    
    The properties which are published in this commit are LocationCode,
    SparePartNumber, Model, SerialNumber and PartNumber.
    
    One of the major use case to publish these properties via redfish
    is for service engineers to identify the FRU and its location in the
    system, which in turn will help them in repair/replacement related to
    that FRU.
    
    The validator has been executed on the change and no error has been
    found.
    As this has been tested on a development image some fields are empty
    in the below pasted output for which warning was thrown by validator but
    no errors.
    
    Sample Output:
    ```
    curl -k -H "X-Auth-Token: $bmc_token" -X GET https://${bmc}/redfish/v1/Chassis/chassis/Assembly/
    {
      "@odata.id": "/redfish/v1/Chassis/chassis/Assembly",
      "@odata.type": "#Assembly.v1_3_0.Assembly",
      "Assemblies": [
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/0",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "0",
          "Model": "",
          "Name": "base_op_panel_blyth",
          "PartNumber": "PN12345",
          "SerialNumber": "YL6B85010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/1",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "1",
          "Model": "",
          "Name": "lcd_op_panel_hill",
          "PartNumber": "PN12345",
          "SerialNumber": "YL6B86010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/2",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "2",
          "Model": "",
          "Name": "tpm_wilson",
          "PartNumber": "PN12345",
          "SerialNumber": "YL6B59010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/3",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "3",
          "Model": "",
          "Name": "vdd_vrm0",
          "PartNumber": "PN12345",
          "SerialNumber": "YL2E32010000",
          "SparePartNumber": ""
        },
        {
          "@odata.id": "/redfish/v1/Chassis/chassis/Assembly#/Assemblies/4",
          "@odata.type": "#Assembly.v1_3_0.AssemblyData",
          "MemberId": "4",
          "Model": "",
          "Name": "vdd_vrm1",
          "PartNumber": "PN12345",
          "SerialNumber": "YL2E32010000",
          "SparePartNumber": ""
        }
      ],
      "Assemblies@odata.count": 5,
      "Id": "Assembly",
      "Name": "Assembly Collection"
    }
    ```
    
    Change-Id: I2d462340fe1a0b0eb387697f0ff70fcafde3f8d9
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>
    Signed-off-by: Ninad Palsule <ninad@linux.ibm.com>
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- redfish-core/lib/assembly.hpp
+++ redfish-core/lib/assembly.hpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
 #pragma once
 
 #include "app.hpp"
@@ -12,6 +14,7 @@
 #include "logging.hpp"
 #include "query.hpp"
 #include "registries/privilege_registry.hpp"
+#include "utils/asset_utils.hpp"
 #include "utils/chassis_utils.hpp"
 #include "utils/dbus_utils.hpp"
 #include "utils/json_utils.hpp"
@@ -44,73 +47,6 @@ namespace redfish
 {
 
 /**
- * @brief Get Asset properties on the given assembly.
- * @param[in] asyncResp - Shared pointer for asynchronous calls.
- * @param[in] serviceName - Service in which the assembly is hosted.
- * @param[in] assembly - Assembly object.
- * @param[in] assemblyIndex - Index on the assembly object.
- * @return None.
- */
-void getAssemblyAsset(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
-                      const auto& serviceName, const auto& assembly,
-                      const auto& assemblyIndex)
-{
-    sdbusplus::asio::getAllProperties(
-        *crow::connections::systemBus, serviceName, assembly,
-        "xyz.openbmc_project.Inventory.Decorator.Asset",
-        [asyncResp, assemblyIndex](
-            const boost::system::error_code& ec1,
-            const dbus::utility::DBusPropertiesMap& propertiesList) {
-            if (ec1)
-            {
-                BMCWEB_LOG_ERROR("DBUS response error {}", ec1.value());
-                messages::internalError(asyncResp->res);
-                return;
-            }
-
-            const std::string* partNumber = nullptr;
-            const std::string* serialNumber = nullptr;
-            const std::string* sparePartNumber = nullptr;
-            const std::string* model = nullptr;
-
-            const bool success = sdbusplus::unpackPropertiesNoThrow(
-                dbus_utils::UnpackErrorPrinter(), propertiesList, "PartNumber",
-                partNumber, "SerialNumber", serialNumber, "SparePartNumber",
-                sparePartNumber, "Model", model);
-
-            if (!success)
-            {
-                messages::internalError(asyncResp->res);
-                return;
-            }
-
-            nlohmann::json& assemblyArray =
-                asyncResp->res.jsonValue["Assemblies"];
-            nlohmann::json& assemblyData = assemblyArray.at(assemblyIndex);
-
-            if (partNumber != nullptr)
-            {
-                assemblyData["PartNumber"] = *partNumber;
-            }
-
-            if (serialNumber != nullptr)
-            {
-                assemblyData["SerialNumber"] = *serialNumber;
-            }
-
-            if (sparePartNumber != nullptr)
-            {
-                assemblyData["SparePartNumber"] = *sparePartNumber;
-            }
-
-            if (model != nullptr)
-            {
-                assemblyData["Model"] = *model;
-            }
-        });
-}
-
-/**
  * @brief Get Location code for the given assembly.
  * @param[in] asyncResp - Shared pointer for asynchronous calls.
  * @param[in] serviceName - Service in which the assembly is hosted.
@@ -125,11 +61,11 @@ void getAssemblyLocationCode(
     sdbusplus::asio::getProperty<std::string>(
         *crow::connections::systemBus, serviceName, assembly,
         "xyz.openbmc_project.Inventory.Decorator.LocationCode", "LocationCode",
-        [asyncResp, assemblyIndex](const boost::system::error_code& ec1,
+        [asyncResp, assemblyIndex](const boost::system::error_code& ec,
                                    const std::string& value) {
-            if (ec1)
+            if (ec)
             {
-                BMCWEB_LOG_ERROR("DBUS response error: {}", ec1.value());
+                BMCWEB_LOG_ERROR("DBUS response error: {}", ec.value());
                 messages::internalError(asyncResp->res);
                 return;
             }
@@ -266,23 +202,17 @@ inline void getAssemblyProperties(
     BMCWEB_LOG_DEBUG("Get properties for assembly associated");
 
     std::size_t assemblyIndex = 0;
-
     for (const auto& assembly : assemblies)
     {
-        nlohmann::json& tempArray = asyncResp->res.jsonValue["Assemblies"];
-
         nlohmann::json::object_t item;
         item["@odata.type"] = "#Assembly.v1_3_0.AssemblyData";
         item["@odata.id"] = boost::urls::format(
             "/redfish/v1/Chassis/{}/Assembly#/Assemblies/{}", chassisId,
             std::to_string(assemblyIndex));
         item["MemberId"] = std::to_string(assemblyIndex);
+        item["Name"] = sdbusplus::message::object_path(assembly).filename();
 
-        tempArray.emplace_back(item);
-
-        tempArray.at(assemblyIndex)["Name"] =
-            sdbusplus::message::object_path(assembly).filename();
-
+        asyncResp->res.jsonValue["Assemblies"].emplace_back(item);
         // Handle special case for tod_battery assembly OEM ReadyToRemove
         // property NOTE: The following method for the special case of the
         // tod_battery ReadyToRemove property only works when there is only ONE
@@ -293,21 +223,20 @@ inline void getAssemblyProperties(
             getReadyToRemoveOfTodBattery(asyncResp, assemblyIndex);
         }
 
+        nlohmann::json::json_pointer assemblyJsonPtr(
+            "/Assemblies/" + std::to_string(assemblyIndex));
+
         dbus::utility::getDbusObject(
             assembly, chassisAssemblyInterfaces,
-            [asyncResp, assemblyIndex,
-             assembly](const boost::system::error_code& ec,
-                       const dbus::utility::MapperGetObject& object) {
+            [asyncResp, assemblyIndex, assembly,
+             assemblyJsonPtr](const boost::system::error_code& ec,
+                              const dbus::utility::MapperGetObject& object) {
                 if (ec)
                 {
                     BMCWEB_LOG_ERROR("DBUS response error : {}", ec.value());
                     messages::internalError(asyncResp->res);
                     return;
                 }
-
-                nlohmann::json::json_pointer ptr(
-                    "/Assemblies/" + std::to_string(assemblyIndex) + "/Name");
-                name_util::getPrettyName(asyncResp, assembly, object, ptr);
 
                 for (const auto& [serviceName, interfaceList] : object)
                 {
@@ -316,8 +245,9 @@ inline void getAssemblyProperties(
                         if (interface ==
                             "xyz.openbmc_project.Inventory.Decorator.Asset")
                         {
-                            getAssemblyAsset(asyncResp, serviceName, assembly,
-                                             assemblyIndex);
+                            asset_utils::getAssetInfo(asyncResp, serviceName,
+                                                      assembly,
+                                                      assemblyJsonPtr);
                         }
                         else if (
                             interface ==
@@ -356,6 +286,37 @@ inline void getAssemblyProperties(
             assemblyArray.size();
 
         assemblyIndex++;
+    }
+}
+
+inline void afterHandleChassisAssemblyGet(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& chassisID, const boost::system::error_code& ec,
+    const std::vector<std::string>& assemblyList)
+{
+    if (ec)
+    {
+        BMCWEB_LOG_WARNING("Chassis not found");
+        messages::resourceNotFound(asyncResp->res, "Chassis", chassisID);
+        return;
+    }
+
+    asyncResp->res.addHeader(
+        boost::beast::http::field::link,
+        "</redfish/v1/JsonSchemas/Assembly/Assembly.json>; rel=describedby");
+
+    asyncResp->res.jsonValue["@odata.type"] = "#Assembly.v1_3_0.Assembly";
+    asyncResp->res.jsonValue["@odata.id"] =
+        boost::urls::format("/redfish/v1/Chassis/{}/Assembly", chassisID);
+    asyncResp->res.jsonValue["Name"] = "Assembly Collection";
+    asyncResp->res.jsonValue["Id"] = "Assembly";
+
+    asyncResp->res.jsonValue["Assemblies"] = nlohmann::json::array();
+    asyncResp->res.jsonValue["Assemblies@odata.count"] = 0;
+
+    if (!assemblyList.empty())
+    {
+        getAssemblyProperties(asyncResp, chassisID, assemblyList);
     }
 }
 
@@ -889,14 +850,44 @@ inline void fillWithAssemblyId(
 
 } // namespace assembly
 
+inline void handleChassisAssemblyHead(
+    crow::App& app, const crow::Request& req,
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const std::string& chassisID)
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+
+    chassis_utils::getChassisAssembly(
+        asyncResp, chassisID,
+        [asyncResp,
+         chassisID](const boost::system::error_code& ec,
+                    const std::vector<std::string>& /*assemblyList*/) {
+            if (ec)
+            {
+                BMCWEB_LOG_WARNING("Chassis not found");
+                messages::resourceNotFound(asyncResp->res, "Chassis",
+                                           chassisID);
+                return;
+            }
+            asyncResp->res.addHeader(
+                boost::beast::http::field::link,
+                "</redfish/v1/JsonSchemas/Assembly.json>; rel=describedby");
+        });
+}
+
 /**
  * Systems derived class for delivering Assembly Schema.
  */
 inline void requestRoutesAssembly(App& app)
 {
-    /**
-     * Functions triggers appropriate requests on DBus
-     */
+    BMCWEB_ROUTE(app, "/redfish/v1/Chassis/<str>/Assembly/")
+        .privileges(redfish::privileges::headAssembly)
+        .methods(boost::beast::http::verb::head)(
+            std::bind_front(handleChassisAssemblyHead, std::ref(app)));
+
     BMCWEB_ROUTE(app, "/redfish/v1/Chassis/<str>/Assembly/")
         .privileges(redfish::privileges::getAssembly)
         .methods(boost::beast::http::verb::get)(
@@ -907,4 +898,5 @@ inline void requestRoutesAssembly(App& app)
         .methods(boost::beast::http::verb::patch)(
             std::bind_front(handleChassisAssemblyPatch, std::ref(app)));
 }
+
 } // namespace redfish
