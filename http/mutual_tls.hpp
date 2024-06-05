7c236c95f5017afd18015ed9e15b670e912c710c
Ed Tanous <ed@tanous.net>
2024-06-05 09:19:06 -0700

    Break out SSL key handler into a compile unit
    This commit allows for no code to have to pull in openssl headers
    directly.  All openssl code is now included in compile units, or
    transitively from boost.
    
    Because http2 is optional, no-unneeded-internal-declaration is needed to
    prevent clang from marking the functions as unused.  Chromium has
    disabled this as well[1]
    
    Tested:
    Redfish service validator passes.
    
    [1] https://issues.chromium.org/issues/40340369
    
    Change-Id: I327e8ffa45941c2282db804d0be56cf64155e67d
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- http/mutual_tls.hpp
+++ http/mutual_tls.hpp
@@ -1,112 +1,15 @@
 #pragma once
 
-#include "logging.hpp"
-#include "mutual_tls_meta.hpp"
 #include "persistent_data.hpp"
-
-extern "C"
-{
-#include <openssl/crypto.h>
-#include <openssl/ssl.h>
-}
 
 #include <boost/asio/ip/address.hpp>
 #include <boost/asio/ssl/verify_context.hpp>
 
 #include <memory>
-#include <span>
+#include <string_view>
 
-inline std::shared_ptr<persistent_data::UserSession>
+std::string getUsernameFromCommonName(std::string_view commonName);
+
+std::shared_ptr<persistent_data::UserSession>
     verifyMtlsUser(const boost::asio::ip::address& clientIp,
-                   boost::asio::ssl::verify_context& ctx)
-{
-    // do nothing if TLS is disabled
-    if (!persistent_data::SessionStore::getInstance()
-             .getAuthMethodsConfig()
-             .tls)
-    {
-        BMCWEB_LOG_DEBUG("TLS auth_config is disabled");
-        return nullptr;
-    }
-
-    X509_STORE_CTX* cts = ctx.native_handle();
-    if (cts == nullptr)
-    {
-        BMCWEB_LOG_DEBUG("Cannot get native TLS handle.");
-        return nullptr;
-    }
-
-    // Get certificate
-    X509* peerCert = X509_STORE_CTX_get_current_cert(ctx.native_handle());
-    if (peerCert == nullptr)
-    {
-        BMCWEB_LOG_DEBUG("Cannot get current TLS certificate.");
-        return nullptr;
-    }
-
-    // Check if certificate is OK
-    int ctxError = X509_STORE_CTX_get_error(cts);
-    if (ctxError != X509_V_OK)
-    {
-        BMCWEB_LOG_INFO("Last TLS error is: {}", ctxError);
-        return nullptr;
-    }
-
-    // Check that we have reached final certificate in chain
-    int32_t depth = X509_STORE_CTX_get_error_depth(cts);
-    if (depth != 0)
-    {
-        BMCWEB_LOG_DEBUG(
-            "Certificate verification in progress (depth {}), waiting to reach final depth",
-            depth);
-        return nullptr;
-    }
-
-    BMCWEB_LOG_DEBUG("Certificate verification of final depth");
-
-    if (X509_check_purpose(peerCert, X509_PURPOSE_SSL_CLIENT, 0) != 1)
-    {
-        BMCWEB_LOG_DEBUG(
-            "Chain does not allow certificate to be used for SSL client authentication");
-        return nullptr;
-    }
-
-    std::string sslUser;
-    // Extract username contained in CommonName
-    sslUser.resize(256, '\0');
-
-    int status = X509_NAME_get_text_by_NID(X509_get_subject_name(peerCert),
-                                           NID_commonName, sslUser.data(),
-                                           static_cast<int>(sslUser.size()));
-
-    if (status == -1)
-    {
-        BMCWEB_LOG_DEBUG("TLS cannot get username to create session");
-        return nullptr;
-    }
-
-    size_t lastChar = sslUser.find('\0');
-    if (lastChar == std::string::npos || lastChar == 0)
-    {
-        BMCWEB_LOG_DEBUG("Invalid TLS user name");
-        return nullptr;
-    }
-    sslUser.resize(lastChar);
-
-    // Meta Inc. CommonName parsing
-    if constexpr (BMCWEB_MUTUAL_TLS_COMMON_NAME_PARSING == "meta")
-    {
-        std::optional<std::string_view> sslUserMeta =
-            mtlsMetaParseSslUser(sslUser);
-        if (!sslUserMeta)
-        {
-            return nullptr;
-        }
-        sslUser = *sslUserMeta;
-    }
-
-    std::string unsupportedClientId;
-    return persistent_data::SessionStore::getInstance().generateUserSession(
-        sslUser, clientIp, unsupportedClientId,
-        persistent_data::SessionType::MutualTLS);
-}
+                   boost::asio::ssl::verify_context& ctx);
--- src/ossl_random.cpp
+++ src/ossl_random.cpp
@@ -1,5 +1,11 @@
 #include "ossl_random.hpp"
 
+extern "C"
+{
+#include <openssl/crypto.h>
+#include <openssl/rand.h>
+}
+
 #include <boost/uuid/random_generator.hpp>
 #include <boost/uuid/uuid_io.hpp>
 
@@ -12,3 +18,48 @@ std::string bmcweb::getRandomUUID()
     return boost::uuids::to_string(
         boost::uuids::basic_random_generator<OpenSSLGenerator>(ossl)());
 }
+
+std::string getRandomIdOfLength(size_t length)
+{
+    static constexpr std::array<char, 62> alphanum = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C',
+        'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c',
+        'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    std::string token;
+    token.resize(length, '0');
+    std::uniform_int_distribution<size_t> dist(0, alphanum.size() - 1);
+
+    bmcweb::OpenSSLGenerator gen;
+
+    for (char& tokenChar : token)
+    {
+        tokenChar = alphanum[dist(gen)];
+        if (gen.error())
+        {
+            return "";
+        }
+    }
+    return token;
+}
+
+bool constantTimeStringCompare(std::string_view a, std::string_view b)
+{
+    // Important note, this function is ONLY constant time if the two input
+    // sizes are the same
+    if (a.size() != b.size())
+    {
+        return false;
+    }
+    return CRYPTO_memcmp(a.data(), b.data(), a.size()) == 0;
+}
+
+bool ConstantTimeCompare::operator()(std::string_view a,
+                                     std::string_view b) const
+{
+    return constantTimeStringCompare(a, b);
+}
+
+} // namespace bmcweb
