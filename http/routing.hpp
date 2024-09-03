openbmc/bmcweb

Fix spelling mistakes using codespell
This commit corrects various spelling mistakes throughout the
repository. The corrections were made automatically using `codespell`[1]
tool.

[1]: https://github.com/codespell-project/codespell

Change-Id: I8751c35ad7246f73ec2e7cd4a04488c25bde3f39
Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>

http/routing.hpp
@@ -272,7 +272,7 @@ class Trie
                     continue;
                 }
 
-                BMCWEB_LOG_CRITICAL("Cant find tag for {}", urlIn);
+                BMCWEB_LOG_CRITICAL("Can't find tag for {}", urlIn);
                 return;
             }
             std::string piece(&c, 1);

baemyung/ibm-bmcweb

Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

http/routing.hpp
@@ -270,7 +270,7 @@ class Trie
                     continue;
                 }
 
-                BMCWEB_LOG_CRITICAL("Cant find tag for {}", url);
+                BMCWEB_LOG_CRITICAL("Can't find tag for {}", url);
                 return;
             }
             std::string piece(&c, 1);